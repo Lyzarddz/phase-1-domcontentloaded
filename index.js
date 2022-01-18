3d42dc80356b78fc5bea6981d7a3d0723ca6e0b8
Alyssa Falcione <lyzarddz@gmail.com>
2022-01-17 19:52:31 -0500

    commit tree 508f50d8b8dcf4035f0f60a001fbd9a369509469

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Your code goes here
 
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("The DOM has loaded");
-    const textEdit = document.querySelector("#text");
+    console.log("The DOM has loaded")
+    const textEdit = document.querySelector("#text")
 
-  textEdit.textContent = "This is really cool!";
-  console.log(textEdit);
+  textEdit.textContent = "This is really cool!"
+  console.log(textEdit)
   });
 
   
